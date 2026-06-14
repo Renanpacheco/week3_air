36f9d34ce6c33cb839974c7debec170d82c0e631
Renanpacheco <prenan86@gmail.com>
2026-06-14 15:01:57 -0300

    update of test cart

--- tests/test_cart_api_serverest.py
+++ tests/test_cart_api_serverest.py
@@ -29,3 +29,57 @@ def test_get_cart_by_id():
     body = response.json()
 
     validate(instance=body, schema=cart_schema)
+
+
+def test_create_cart_without_token():
+    payload = {
+        "nome": "carrinho 024578",
+        "preco": 470,
+        "descricao": "Mouse",
+        "quantidade": 381
+    }
+    response = requests.post(f"{BASE_URL}/carrinhos", json=payload)
+    assert response.status_code == 401
+    body = response.json()    
+    assert body["message"] == "Token de acesso ausente, inválido, expirado ou usuário do token não existe mais"
+
+
+def test_create_cart_with_invalid_token():
+    
+    headers = {
+        "Authorization": "bearer invalid_token"
+    }
+    
+    name_product = f"mouse{int(time.time() * 1000)}"
+    payload = {
+        "nome": name_product,
+        "preco": 470,
+        "descricao": "Mouse",
+        "quantidade": 381
+    }
+
+    response = requests.post(f"{BASE_URL}/produtos", headers=headers, json=payload)
+    assert response.status_code == 401
+    
+    body = response.json()
+    assert body["message"] == "Token de acesso ausente, inválido, expirado ou usuário do token não existe mais"
+
+@pytest.mark.skip()    
+def test_create_cart_without_name(token_autentication):
+    headers = {
+        "Authorization": token_autentication
+    }
+    
+    payload = {
+        
+        "preco": 470,
+        "descricao": "Mouse",
+        "quantidade": 381
+    }
+    
+    response = requests.post(f"{BASE_URL}/produtos", headers=headers, json=payload)
+    assert response.status_code == 400
+
+    body = response.json()
+    assert body["nome"] == "nome é obrigatório"
+
